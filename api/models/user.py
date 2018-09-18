32845d235d8a35ded86b87716ca1652040641499
naibor <liznaibor.en@gmail.com>
2018-09-18 11:31:46 +0300

    {feature 160131887}streamline thoughts

--- api/models/user.py
+++ api/models/user.py
@@ -1,6 +1,6 @@
 # Note from 2018-08-29 16:17:23.601
 from app import db
-from passlib.hash import pbkdf2_sha256
+from werkzeug.security import generate_password_hash, check_password_hash
 class NormalUser():
     # user model class
     def __init__(self, username, email, password, confirm_password):
@@ -23,31 +23,32 @@ class NormalUser():
         # return {"messege":"successfully signed up, awaiting approval. be on the look out for a verification email"}
         pass
 
-@static method
-# check if password and confirm password match
-def matching_and_hashing(password,confirm_password):
-    if self.password != self.confirm_password:
-        return {"message":"password and confirm password should be the same"}
-    else:
-        password = pbkdf2_sha256.hash(password)
-        return password
 
-    pass
+    # check if password and confirm password match
+    def matching_and_hashing(self):
+        if self.password != self.confirm_password:
+            return {"message":"password and confirm password should be the same"}
+        else:
+            self.password = generate_password_hash(self.password,method="sha256")
+
+
+        pass
 
 
 # approved users are able to login >> verified users are able to login
     def logging_in_normal_user():
-        # if verified is true:
+        # if approved is true:
         # check if credentials provided are correct using correct_credentials()
         pass
 # when logging in check if correct password is provided
-    def correct_credentials():
-        # select from users table where email = email provided
-        # then fetchone() = user info
-        # if its actually this users info and the hashed password = stored password,
-        # grant this user access tokens
-        # else {"message":"wrong password provided"}
-        pass
+@staticmethod
+def correct_credentials(username, password):
+    # select from users table where email = email provided
+    # then fetchone() = user info
+    # if this users info and the check password hash
+    # grant this user access tokens
+    # else {"message":"wrong password provided"}
+    pass
 
 
 class Admin(NormalUser):
