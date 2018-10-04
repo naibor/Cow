4ea553e279049947575d84c9b09f91c9a6547bda
naibor <liznaibor.en@gmail.com>
2018-10-04 19:10:36 +0300

    [Feature #160988490]user can view created cows

---
+++ api/tests/test_view_all_cow.py
@@ -0,0 +1,38 @@
+import unittest
+import json
+from datetime import datetime
+from app.endpoints.cow_construction import ConstructionProcess
+from models.cow_model import CowAssemblyModel
+
+from tests import BaseTestCase
+
+assembly_time =datetime.now()
+
+class Test_Cow_Construction(BaseTestCase):
+    """test cow construction"""
+
+    def test_save_milk(self):
+        """test a user can create a cow"""
+        cow =self.test_client.post(
+            "/api/v1/cow",
+            data = json.dumps(dict(
+                moo_name = "Bronze",
+                breed = "Bull",
+                age = "2",
+                cow_health="Needs a mate",
+                time = str(assembly_time)
+
+                )),
+            headers = {"content-type":"application/json"}
+            )
+        # self.assertEqual(cow.status_code,201)
+
+        # get the cows
+        get_cows = self.test_client.get("/api/v1/cow",
+                headers = {"content-type":"application/json"}
+            )
+
+        # import pdb; pdb.set_trace()
+        self.assertEqual(get_cows.status_code,200 )
+
+
