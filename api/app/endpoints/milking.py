4d419f6a37c09048c190bea2fffa8715e7a69b67
naibor <liznaibor.en@gmail.com>
2018-10-12 12:42:35 +0300

    [Feature #161177103] update api documentation

--- api/app/endpoints/milking.py
+++ api/app/endpoints/milking.py
@@ -10,6 +10,8 @@ from flask_restplus import Resource
 from flask_jwt_extended import jwt_required, jwt_refresh_token_required, get_jwt_identity
 from models.milk import MilkingModel
 from models.schema import Milkschema
+from app.serializer import milking
+
 milk_ns = API.namespace('milk',
                         description="Milk entry/milk entry operations.",
                         path='/cow/<int:cow_id>/milk')
@@ -31,6 +33,7 @@ class MilkingProcess(Resource):
     """milk entries resource"""
 
     # post milk entry for a particular cow
+    @API.expect(milking)
     @jwt_required
     def post(self, cow_id):
         current_user = get_jwt_identity()
