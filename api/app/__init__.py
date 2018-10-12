09179629e42918b8ed9328155a526a8576a08dcc
naibor <liznaibor.en@gmail.com>
2018-10-12 14:32:30 +0300

    [Feature #161177103]fix failing build

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -15,9 +15,8 @@ APP.config.from_object(app_config[config_name])
 
 APP.config['PROPAGATE_EXCEPTIONS'] = True
 jwt_manager = JWTManager()
-jwt_manager = JWTManager(APP)
 jwt_manager.init_app(APP)
-
+jwt_manager = JWTManager(APP)
 jwt_manager._set_error_handler_callbacks(APP)
 
 # overide 404 error handler
